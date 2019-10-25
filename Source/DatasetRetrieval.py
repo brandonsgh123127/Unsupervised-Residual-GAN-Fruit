4241b89babebabe0492c22466a411281ec95a61b
Brandon Spada <brandonsgh123127@gmail.com>
2019-10-24 23:42:11 -0400

    -DataSetRetrieval now actually sets an array to point to specific names in a dataset -As of now, only fruits in test array
    To Do:
    -Edge detection using OpenCV...
    -Implement a generator/descriminator
    -Plotting data to pixel values for manipulation...
    -Etc.

--- Source/DatasetRetrieval.py
+++ Source/DatasetRetrieval.py
@@ -2,6 +2,8 @@ import matplotlib
 import pandas as pd
 import sklearn
 import skimage
+import os
+import cv2 #USED FOR EDGE DETECTION IN IMAGES
 
 import numpy as np
 
@@ -18,17 +20,24 @@ class DatasetRetrieval:
     # Used to initialize the class/Object when created
     def __init__(self):
         self.testSize = 250
-        self.imArray= np.zeros((1,self.testSize))
+        self.imArray=np.zeros((self.testSize,1))
+        self.imArray=self.imArray.astype(str)
+        self.compArray=np.zeros(())
+
     """
     Used to retrieve a random set of images in dataset...
     """
     def retrieveImages(self):
         # Reads file named testPhotos, which points to each photo name
-        with open('trainPhotos.txt','w')as f:
+        with open(os.getcwd()[:-7]+'\\FileNames\\fruitNames.txt','r')as f:
+            content = f.readlines()
             # Retrieve random number of objects in dataset to train model...
             for item in range(0,self.testSize):
-                print("To be implemented")
-                f.write("s")
+                rand = np.random.randint(0,high=28590)
+                self.imArray[item]= content[rand]  #imArray contains full quality image set locations
+                w = open('E:\\Users\\i-pod\\Desktop\\Projects_CS\\Python\\Fruit-Images\\Fruit-Images-Dataset-master\\BasicFruit Images\\'+self.imArray[item])
+
+            print("Testing... ",self.imArray[self.testSize-1])
 
 
 start = DatasetRetrieval()
